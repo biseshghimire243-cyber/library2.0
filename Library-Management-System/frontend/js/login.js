9ec06d5c5c2150a563162db44686776004a41855
Bishesh Ghimire <biseshghimire243@gmail.com>
2026-08-02 05:22:00 -0700

    the js are added well

---
+++ Library-Management-System/frontend/js/login.js
@@ -0,0 +1,77 @@
+// ===============================
+// LOGIN
+// ===============================
+
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const username = document.getElementById("username").value.trim();
+
+    const password = document.getElementById("password").value.trim();
+
+    const message = document.getElementById("message");
+
+    message.style.color = "red";
+    message.textContent = "";
+
+    try {
+
+        const response = await fetch("http://localhost:3000/login", {
+
+            method: "POST",
+
+            headers: {
+
+                "Content-Type": "application/json"
+
+            },
+
+            body: JSON.stringify({
+
+                username,
+                password
+
+            })
+
+        });
+
+        const result = await response.json();
+
+        if(result.success){
+
+            message.style.color = "green";
+
+            message.textContent = result.message;
+
+            // Save login status
+            localStorage.setItem("loggedIn", "true");
+
+            // Redirect after 1 second
+            setTimeout(() => {
+
+                window.location.href = "index.html";
+
+            },1000);
+
+        }
+
+        else{
+
+            message.textContent = result.message;
+
+        }
+
+    }
+
+    catch(error){
+
+        console.error(error);
+
+        message.textContent = "Server connection failed.";
+
+    }
+
+});
